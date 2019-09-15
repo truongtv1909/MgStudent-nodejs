47fc588907f7a00a310b943e1dbd3b52c9f540e4
truongtv1909 <truongtv1909@gmail.com>
2019-09-15 13:50:33 +0700

    fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,12 +15,56 @@ module.exports.getSearch = function(req,res){
     let sql = "select * from user";
     con.query(sql,function(err,data){
         if(err) throw err;
-        // console.log(data);
         let find = data.filter(function(item){
-            return item.name.includes(key);
+            return item.name.toLowerCase().includes(key.toLowerCase())
+                || item.email.toLowerCase().includes(key.toLowerCase());
         })
         res.render('user/index',{
             users:find
-        })
+        });
+    });
+}
+
+module.exports.getDetail = function(req,res){
+    let id = req.params.userId;
+    let sql = `select * from user where id = ${id}`;
+    con.query(sql,function(err,data){
+        if(err) throw err;
+        res.render('user/detail',{
+            user:data[0]
+        });
+    });
+}
+
+module.exports.getRemove = function(req,res){
+    let id = req.params.userId;
+    let sql = `DELETE FROM user WHERE id = ${id}`;
+    con.query(sql,function(err,data){
+        if(err) throw err;
+        res.redirect('/user');
+    });
+}
+
+module.exports.getCreate = function(req,res){
+    res.render('user/create');
+}
+
+module.exports.postCreateUser = function(req,res){
+    let data = req.body;
+    let sql = `INSERT INTO user(name, age, email, password, logo) 
+    VALUES ('${data.name}','${data.age}','${data.email}','${data.password}','${data.logo}') `;
+    con.query(sql,function(err,datas){
+        if(err) throw err;
+        res.redirect('/user');
     })
+}
+
+module.exports.postUpdateUser = function(req, res){
+    let value = req.body;
+    let sql = `UPDATE user SET name = '${value.name}',age = ${value.age},email = '${value.email}',
+    password = '${value.password}',logo = '${value.logo}'  WHERE id = ${value.id}`;
+    con.query(sql,function(err,data){
+        if(err) throw err;
+        res.redirect('/user');
+    });
 }
--- rountes/user.rounter.js
+++ rountes/user.rounter.js
@@ -4,6 +4,11 @@ const userController = require('../controllers/user.controller');
 
 rounter.get('/',userController.getIndex);
 rounter.get('/search',userController.getSearch);
+rounter.get('/detail/:userId',userController.getDetail);
+rounter.get('/remove/:userId',userController.getRemove);
+rounter.get('/create',userController.getCreate);
 
+rounter.post('/create',userController.postCreateUser);
+rounter.post('/update',userController.postUpdateUser);
 
 module.exports = rounter;
